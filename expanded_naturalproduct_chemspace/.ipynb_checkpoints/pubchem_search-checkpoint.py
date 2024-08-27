1a1bdd36ec2fd434deef1745fcea784eaedd764b
pamrein <pascal.amrein@unifr.ch>
2024-08-27 15:50:50 +0200

    pubchem

--- expanded_naturalproduct_chemspace/.ipynb_checkpoints/pubchem_search-checkpoint.py
+++ expanded_naturalproduct_chemspace/.ipynb_checkpoints/pubchem_search-checkpoint.py
@@ -1,11 +1,9 @@
 import pubchempy as pcp
 import csv, os
-import polars as pl
 
 df_mongo = pl.read_parquet("../data/MINES/mongo_predicted_compounds.parquet")
 file = "../data/taxonomy/smiles_cid.tsv"
 smiles = df_mongo["SMILES_mongo"].unique().to_list()
-
 
 # Header to add
 header = ['smile', 'cid_compound']
@@ -20,22 +18,27 @@ else:
     print(f"The file '{file}' already exists.")
     
 
+total_length = len(smiles)
+i = 0
+
 for smile in smiles:
     c = pcp.get_compounds(smile, 'smiles')
-
+    
     for compound in c:
         cid_compound = compound.cid
-                    
-        print(cid_compound)
-        
+                
         if cid_compound == None:
-            print("nothing found")
+            continue;
         else:
             additional_data = [
                 [smile, cid_compound],
             ]
             
             # Write data to a TSV file
-            with open(filename, 'a', newline='') as file:
+            with open(file, 'a', newline='') as file:
                 writer = csv.writer(file, delimiter='\t')
                 writer.writerows(additional_data)
+
+
+    i = i + 1
+    print(f"{i}/{total_length} - {cid_compound}")
